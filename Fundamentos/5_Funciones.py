d47ea6aee2b76a10fe28922182a290a11e3c3db9
Michael Felipe <mayala@trueblue.com>
2021-03-10 12:58:13 -0500

    Actualizar ejemplos de reuso de funciones python

--- Fundamentos/5_Funciones.py
+++ Fundamentos/5_Funciones.py
@@ -3,6 +3,18 @@ max_valor = max(10,15,30,12,8,19)
 print("max_valor: ", max_valor)
 
 # Funcion para obtener el mínimo valor.
-min_valor = max(10,15,30,12,8,19)
+min_valor = min(10,15,30,12,8,19)
 print("min_valor: ", min_valor)
 
+# Funcion para reemplazar una palabra
+mensaje = "Python es muy facil"
+print(mensaje.replace('facil', 'xD'))
+
+# Funcion para cambiar el texto a mayúsculas
+nombre = "aureliano"
+print(nombre.upper())
+
+# Funcion para mostrar la lista invertida
+posiciones = [1,2,3]
+posiciones.reverse()
+print(posiciones)
